a43330ded14d9ee0f56da303e97fba914caaa634
VadimKTS <123896357+VadimKTS@users.noreply.github.com>
2023-11-10 01:56:11 +0300

    байтовый поток

--- Program.cs
+++ Program.cs
@@ -1,10 +1,22 @@
-﻿namespace Test_ConsoleApp
+﻿using System.Text;
+using Test_StreamReader;
+namespace Test_ConsoleApp
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            char delimiter;
+            Console.WriteLine("Введите разделитель");
+            delimiter = (char)Console.Read();
+            Console.WriteLine($"Разделитель - \"{delimiter}\"");
+            var message = MessageReader.ReadMessages(delimiter);
+
+
+            foreach (var line in message)
+            {
+                Console.WriteLine(Encoding.Default.GetString(line));
+            }
         }
     }
 }
